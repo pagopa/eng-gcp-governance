9b93e3431ae3b9997d988a1ceb15d7bb28af49fc
maurodandrea <98483756+maurodandrea@users.noreply.github.com>
2025-04-09 13:14:56 +0200

    example-allowed-locations-json

--- src/01_policy_allowed_locations/01_allowed_locations.tf
+++ src/01_policy_allowed_locations/01_allowed_locations.tf
@@ -16,7 +16,26 @@ resource "google_org_policy_policy" "resource_allowed_locations" {
   }
 }
 
-module "module_allowed_locations" {
+module "module_allowed_locations_project" {
+  source  = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
+  version = "7.0.0"
+
+  policy_root    = "project" # organization, folder or project
+  policy_root_id = var.project_id
+  constraint     = "constraints/gcp.resourceLocations"
+  policy_type    = "list"
+
+  # rules = [
+  #   {
+  #     enforcement = null
+  #     allow       = ["in:eu"]
+  #   }
+  # ]
+
+  rules = [jsondecode(file("policy_rules/allow_locations_project.json"))]
+}
+
+module "module_allowed_locations_folder" {
   source  = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
   version = "7.0.0"
 
@@ -25,10 +44,31 @@ module "module_allowed_locations" {
   constraint     = "constraints/gcp.resourceLocations"
   policy_type    = "list"
 
-  rules = [
-    {
-      enforcement = null
-      allow       = ["in:eu"]
-    }
-  ]
+  # rules = [
+  #   {
+  #     enforcement = null
+  #     allow       = ["in:eu"]
+  #   }
+  # ]
+
+  rules = [jsondecode(file("policy_rules/allow_locations_folder.json"))]
+}
+
+module "module_allowed_locations_organization" {
+  source  = "terraform-google-modules/org-policy/google//modules/org_policy_v2"
+  version = "7.0.0"
+
+  policy_root    = "organization" # organization, folder or project
+  policy_root_id = var.organization_id
+  constraint     = "constraints/gcp.resourceLocations"
+  policy_type    = "list"
+
+  # rules = [
+  #   {
+  #     enforcement = null
+  #     allow       = ["in:eu"]
+  #   }
+  # ]
+
+  rules = [jsondecode(file("policy_rules/allow_locations_organization.json"))]
 }
