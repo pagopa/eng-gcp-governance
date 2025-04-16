1c310f3f58ec3a05c75c8cf28c3b5882c77a7b34
Pasquale De Vita <pasquale.devita@pagopa.it>
2025-04-16 16:34:52 +0200

    add custom exception

---
+++ src/03_policy_assignments/modules/custom/01_iam_managed_disableServiceAccountKeyCreation.tf
@@ -0,0 +1,24 @@
+locals {
+  iam_managed_disableServiceAccountKeyCreation = {
+    prod-ict-513635 = {
+      enforce = "FALSE"
+    },
+    organization-443016 = {
+      enforce = "FALSE"
+    },
+  }
+}
+
+resource "google_org_policy_policy" "iam_managed_disableServiceAccountKeyCreation" {
+  for_each = local.iam_managed_disableServiceAccountKeyCreation
+  name   = "projects/${each.key}/policies/iam.managed.disableServiceAccountKeyCreation"
+  parent = "projects/${each.key}"
+
+  spec {
+    inherit_from_parent = false
+
+    rules {
+      enforce = each.value.enforce
+    }
+  }
+}
