bc2b8ed3dd98d96efabfea334498b4054ed7a760
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2025-05-12 09:57:00 +0200

    add custom role scaffolder (#6)

---
+++ src/01_custom_roles/variables.tf
@@ -0,0 +1,22 @@
+variable "organization_id" {
+  type    = string
+  default = "520838205140"
+}
+
+variable "project_id" {
+  description = "ProjectID Google Cloud"
+  type        = string
+  default     = "organization-443016"
+}
+
+variable "region" {
+  description = "Region Google Cloud"
+  type        = string
+  default     = "europe-west8"
+}
+
+variable "zone" {
+  description = "Zone Google Cloud"
+  type        = string
+  default     = "europe-west8-a"
+}
